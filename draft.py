5415f6a78b97b207140cd200e24de3bbf9602af2
EmelyanStepanov <emelian.sstepanov@gmail.com>
2022-07-15 16:46:22 +0300

    я рот ебал гита

--- draft.py
+++ draft.py
@@ -1,1 +1,18 @@
-print ("My name is Emelyan")
+
+violator_songs_dict = {
+    'World in My Eyes': 4.76,
+   'Sweetest Perfection': 4.43,
+    'Personal Jesus': 4.56,
+    'Halo': 4.30,
+    'Waiting for the Night': 6.07,
+    'Enjoy the Silence': 4.6,
+    'Policy of Truth': 4.88,
+    'Blue Dress': 4.18,
+    'Clean': 5.68,
+}
+
+# распечатайте общее время звучания трех песен: 'Sweetest Perfection', 'Policy of Truth' и 'Blue Dress'
+#   А другие три песни звучат ХХХ минут
+
+print (round(violator_songs_dict["Sweetest Perfection"] + violator_songs_dict["Policy of Truth"] + violator_songs_dict["Blue Dress"],2))
+print ("А другие песни звучат", round(violator_songs_dict["World in My Eyes"] + violator_songs_dict["Personal Jesus"] + violator_songs_dict["Waiting for the Night"],2))
